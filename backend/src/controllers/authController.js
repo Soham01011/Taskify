945c7dd99f7362a9bc5386bb22b54dbc412ddde3
Soham Dalvi <crafterking@tutanota.com>
2025-04-20 13:36:14 +0530

    basic api for login and registration

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,54 @@
+// src/controllers/authController.js
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// Register user
+exports.registerUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Create a new user
+    const user = new User({ username, password });
+    await user.save();
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(500).json({ message: 'Error registering user' });
+  }
+};
+
+// Login user
+exports.loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    // Compare passwords
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: 'Error logging in user' });
+  }
+};
---
+++ backend/src/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');   
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 20,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    }
+});
+
+UserSchema.pre('save', async function(next) {
+    if ( this .isModified  ('password') || this.isNew ) {
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+    next();
+});
+
+UserSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+module.exports = mongoose.model('User', UserSchema);
